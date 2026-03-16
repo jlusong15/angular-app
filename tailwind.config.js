f0a4f14f7ac4f566a585d566959e4a607ed6f4d6
Mochi <mochi@egghead-2.local>
2026-03-16 16:01:36 +0800

    setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+        success: 'var(--success)',
+        destructive: 'var(--destructive)',
+        neutralGray: {
+          0: 'var(--neutralGray-0)',
+          100: 'var(--neutralGray-100)',
+          200: 'var(--neutralGray-200)',
+          300: 'var(--neutralGray-300)',
+          400: 'var(--neutralGray-400)',
+          500: 'var(--neutralGray-500)',
+          600: 'var(--neutralGray-600)',
+          700: 'var(--neutralGray-700)',
+          800: 'var(--neutralGray-800)',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
