100679feaef3dcfba8973892a57736dc2216f238
Mochi <mochi@egghead-2.local>
2026-03-17 14:33:10 +0800

    Add date picker

--- src/app/pages/dashboard/kpi-details/kpi-details.ts
+++ src/app/pages/dashboard/kpi-details/kpi-details.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { DatePipe } from '@angular/common';
-import { DatePicker } from '@shared/component/date-picker/date-picker';
+import { DatePicker } from 'primeng/datepicker';
+import { Subject, takeUntil } from 'rxjs';
+// import { DatePicker } from '@shared/component/date-picker/date-picker';
 
 @Component({
   selector: 'app-kpi-details',
-  imports: [DatePicker, FormsModule, DatePipe],
+  imports: [FormsModule, DatePipe, ReactiveFormsModule, DatePicker],
   templateUrl: './kpi-details.html',
   styleUrl: './kpi-details.css',
   standalone: true
 })
 export class KpiDetails {
-  selectedDate: Date = new Date();
+  private destroy$ = new Subject<void>();
+  date: Date | undefined = new Date();
+  form = new FormGroup({
+    selectedDate: new FormControl('')
+  });
+
+  constructor(private fb: UntypedFormBuilder) {
+    this.form = this.fb.group({
+      selectedDate: [new Date()]
+    });
+
+    this.form.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+
 
   onDateSelected(date: Date) {
     console.log('Date selected:', date);
