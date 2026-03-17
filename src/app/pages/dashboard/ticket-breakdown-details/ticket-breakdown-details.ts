be8900db214bbb9e7eae4a1aa2349789ff23f7ea
Mochi <mochi@egghead-2.local>
2026-03-17 19:29:59 +0800

    Create form-select reactive form from primeng p-select
    Co-Authored-By: Jennifer Bautista <72844242+jlusong15@users.noreply.github.com>

--- src/app/pages/dashboard/ticket-breakdown-details/ticket-breakdown-details.ts
+++ src/app/pages/dashboard/ticket-breakdown-details/ticket-breakdown-details.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormSelect } from '@shared/component/form-select/form-select';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder } from "@angular/forms";
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-ticket-breakdown-details',
-  imports: [FormSelect],
+  imports: [FormSelect, FormsModule, ReactiveFormsModule],
   templateUrl: './ticket-breakdown-details.html',
   styleUrl: './ticket-breakdown-details.css',
   standalone: true
 })
-export class TicketBreakdownDetails {}
+export class TicketBreakdownDetails implements OnDestroy {
+  private destroy$ = new Subject<void>();
+  projectDropdown = [
+    {
+      name: 'Project 1',
+      code: 'project1'
+    },
+    {
+      name: 'Project 2',
+      code: 'project2'
+    },
+    {
+      name: 'Project 3',
+      code: 'project3'
+    },
+    {
+      name: 'Project 4',
+      code: 'project4'
+    },
+    {
+      name: 'Project 5',
+      code: 'project5'
+    },
+  ]
+
+  form = new FormGroup({
+    project: new FormControl('')
+  });
+
+
+  constructor(private fb: UntypedFormBuilder) {
+    this.form = this.fb.group({
+      project: ['']
+    });
+
+    this.form.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((x) => {
+        console.log(x);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+}
--- src/app/shared/component/form-select/form-select.ts
+++ src/app/shared/component/form-select/form-select.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormBuilder, Validators } from '@angular/forms';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, forwardRef, HostBinding, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormControl, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
 import { SelectModule } from 'primeng/select';
-
-interface City {
+import { Subject, takeUntil } from 'rxjs';
+interface FormSelectModel {
   name: string;
   code: string;
 }
@@ -13,33 +13,82 @@ interface City {
   templateUrl: './form-select.html',
   styleUrl: './form-select.css',
   standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormSelect),
+      multi: true,
+    },
+  ],
 })
-export class FormSelect {
-  data: City[] = [];
+export class FormSelect implements ControlValueAccessor, AfterViewInit {
+  private destroy$ = new Subject<void>();
+  @Input() list: FormSelectModel[] = [];
+  @Input() placeholder: string = 'Select';
+  @Input() disabled: boolean = false;
+
+  @Output() onValueChanged = new EventEmitter<any>();
+
+  @HostBinding('attr.tabindex') attrTabindex = null;
+  @HostBinding('attr.id') attrId = null;
+  @HostBinding('attr.disabled') attrDisabled = null;
+
   form: FormGroup = new FormGroup({
-    city: new FormControl('')
+    select: new FormControl('')
   });
   formSubmitted: boolean = false;
+  isDisabled: boolean = false;
 
-
-  constructor(private fb: UntypedFormBuilder) {
+  constructor(private fb: UntypedFormBuilder, private cd: ChangeDetectorRef) {
     this.form = this.fb.group({
-      city: ['', Validators.required]
+      select: ['']
     });
   }
 
-  onSubmit() {
-    this.formSubmitted = true;
-    if (this.form?.valid) {
-      console.log(this.form)
-      // this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form is submitted', life: 3000 });
-      this.form?.reset();
-      this.formSubmitted = false;
-    }
+  ngAfterViewInit(): void {
+    this.cd.detectChanges();
+
+    this.form.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((x) => this.onChange(x));
   }
 
-  isInvalid(controlName: string) {
-    const control = this.form.get(controlName);
-    return control?.invalid && (control.touched || this.formSubmitted);
+  value: any;
+
+  onChange: any = () => { };
+  onTouched: any = () => { };
+
+  writeValue(obj: any): void {
+    console.log('write', obj)
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  handleChange(event: any) {
+    console.log('handleChange', event)
+    this.value = event.value;
+    this.onChange(this.value);
+    this.onTouched();
+    this.onValueChanged.emit(this.value);
+  }
+
+  get computedDisabled(): boolean {
+    return this.disabled || this.isDisabled;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
