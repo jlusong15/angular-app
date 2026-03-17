1f525f2fdb4b997d90373167bb39bebc4b23ad1f
Mochi <mochi@egghead-2.local>
2026-03-17 21:35:49 +0800

    Apply data randomizer on bar chart
    Co-Authored-By: Jennifer Bautista <72844242+jlusong15@users.noreply.github.com>

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -4,6 +4,7 @@ import { BarChart } from '@shared/component/bar-chart/bar-chart';
 import { KpiDetails } from './kpi-details/kpi-details';
 import { CalendarDays, LucideAngularModule } from 'lucide-angular';
 import { TicketBreakdownDetails } from "./ticket-breakdown-details/ticket-breakdown-details";
+import { generateRandomDataset } from '@shared/utils/util';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,17 +18,26 @@ export class Dashboard {
     CalendarDays
   }
   kpiData: DataItem[] = [
-    { name: 'Jan', value: 120 },
-    { name: 'Feb', value: 150 },
-    { name: 'Mar', value: 180 },
-    { name: 'Apr', value: 170 },
-    { name: 'May', value: 210 },
-    { name: 'Jun', value: 250 },
-    { name: 'Jul', value: 230 },
-    { name: 'Aug', value: 260 },
-    { name: 'Sep', value: 240 },
-    { name: 'Oct', value: 280 },
-    { name: 'Nov', value: 300 },
-    { name: 'Dec', value: 350 }
+    { name: 'Jan', value: 0 },
+    { name: 'Feb', value: 0 },
+    { name: 'Mar', value: 0 },
+    { name: 'Apr', value: 0 },
+    { name: 'May', value: 0 },
+    { name: 'Jun', value: 0 },
+    { name: 'Jul', value: 0 },
+    { name: 'Aug', value: 0 },
+    { name: 'Sep', value: 0 },
+    { name: 'Oct', value: 0 },
+    { name: 'Nov', value: 0 },
+    { name: 'Dec', value: 0 }
   ];
+
+  constructor() {
+    this.refreshDataset();
+  }
+
+  refreshDataset() {
+    const data = generateRandomDataset(12)
+    this.kpiData = this.kpiData?.map((x, i) => ({ ...x, value: data?.[i] }))
+  }
 }
