be63d1b067889f00d6e6774bfcba087eed482ec4
Mochi <mochi@egghead-2.local>
2026-03-20 14:47:27 +0800

    Initial development for contact page
    Co-Authored-By: Jennifer Bautista <72844242+jlusong15@users.noreply.github.com>

--- src/app/pages/contact-me/contact-me.ts
+++ src/app/pages/contact-me/contact-me.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Breadcrumb } from '@shared/component/breadcrumb/breadcrumb';
 import { RightSidebar } from '@shared/component/right-sidebar/right-sidebar';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [RightSidebar],
+  imports: [RightSidebar, Breadcrumb],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.css',
   standalone: true,
 })
-export class ContactMe {}
+export class ContactMe {
+  readonly breadcrumbs = [{ label: 'Contact' }]
+}
